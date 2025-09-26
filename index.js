c81f337ec86b7a69027920ba5e1c8e980b7eb2d0
PE8RI <ritwickraj5632@gmail.com>
2025-09-26 22:36:55 +0530

    understand the concept of static file

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-const express=require("express");
-const path=require("path");
-const app=express();
-const port=8080;
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8080;
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.get("/",(req,res)=>{
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Serve static files from public
+app.use(express.static(path.join(__dirname, "public/css")));
+app.use(express.static(path.join(__dirname, "public/js")));
+
+app.get("/", (req, res) => {
   res.render("home.ejs");
-})
+});
 
-app.get("/home",(req,res)=>{
-  res.send("hi, This is my page");
-})
+app.get("/home", (req, res) => {
+  res.send("Hi, this is my page");
+});
 
-app.get("/ig/:username",(req,res)=>{
-  let {username}=req.params;
-  const instaData=require("./data.json");
-  const data=instaData[username];
-  if(data){
-  res.render("instagram.ejs",{data});
-  }
-  else{
+app.get("/ig/:username", (req, res) => {
+  let { username } = req.params;
+  const instaData = require("./data.json");
+  const data = instaData[username];
+
+  if (data) {
+    res.render("instagram.ejs", { data });
+  } else {
     res.render("error.ejs");
   }
-})
+});
 
-app.get("/rolldice",(req,res)=>{
-  let dice=Math.floor(Math.random()*10)+10;
-  res.render("rolldice.ejs",{dice});
-})
-app.listen(port,()=>{
-  console.log(`listenng to port no ${port}`);
-})
+app.get("/rolldice", (req, res) => {
+  let dice = Math.floor(Math.random() * 10) + 1;
+  res.render("rolldice.ejs", { dice });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
